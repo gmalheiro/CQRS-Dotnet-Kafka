acec458fb19f07d7449892f1894187738408f867
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2023-07-13 18:29:34 -0300

    Create AggregateRoot class

---
+++ Code/CQRS-ES/CQRS.Core/Domain/AggregateRoot.cs
@@ -0,0 +1,19 @@
+﻿using CQRS.Core.Events;
+using System;
+
+namespace CQRS.Core.Domain
+{
+    public abstract class AggregateRoot
+    {
+        protected Guid _id;
+        
+        private readonly List<BaseEvent> _changes = new();
+        
+        public Guid Id
+        {
+            get { return _id; }
+        }
+
+        public int Version { get; set; } = -1;
+    }
+}
