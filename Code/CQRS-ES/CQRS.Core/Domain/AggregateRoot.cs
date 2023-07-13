d0e1ba593decc85ae9021fcbf90de4c4c6d8f361
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2023-07-13 18:47:20 -0300

    AggregateRoot done

--- Code/CQRS-ES/CQRS.Core/Domain/AggregateRoot.cs
+++ Code/CQRS-ES/CQRS.Core/Domain/AggregateRoot.cs
@@ -15,5 +15,45 @@ namespace CQRS.Core.Domain
         }
 
         public int Version { get; set; } = -1;
+
+        public IEnumerable<BaseEvent> GetUncommittedChanges()
+        {
+            return _changes;
+        }
+
+        public void MarkChangesAsCommitted()
+        {
+            _changes.Clear();
+        }
+
+        private void ApplyChanges(BaseEvent @event, bool isNew)
+        {
+            var method = this. GetType().GetMethod("Apply",new Type[] {@event.GetType() });
+
+            if (method == null)
+            {
+                throw new ArgumentNullException(nameof(method), $"The Apply method was not found in the aggregate for {@event.GetType().Name}!");
+            }
+
+            method.Invoke(this, new object[] { @event });
+
+            if (isNew)
+            {
+                _changes.Add( @event );
+            }
+        }
+
+        protected void RaiseEvent(BaseEvent @event)
+        {
+            ApplyChanges(@event, true);
+        }
+
+        public void ReplayEvents(IEnumerable<BaseEvent> events)
+        {
+            foreach (var @event in events)
+            {
+                ApplyChanges(@event, false);
+            }
+        }
     }
 }
