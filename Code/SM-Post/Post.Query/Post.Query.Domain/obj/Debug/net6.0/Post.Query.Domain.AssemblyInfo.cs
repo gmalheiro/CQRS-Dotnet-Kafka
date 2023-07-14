ecbdc060cb680c8431cd1e5f62f518d16eca225c
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2023-07-14 16:46:57 -0300

    Added event sourcing handler reference in program.cs

--- Code/SM-Post/Post.Query/Post.Query.Domain/obj/Debug/net6.0/Post.Query.Domain.AssemblyInfo.cs
+++ Code/SM-Post/Post.Query/Post.Query.Domain/obj/Debug/net6.0/Post.Query.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Post.Query.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c0b6653b4c2e92b539b6a38421892a572870ded")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b9bd9b82742a6afa67ccfab1a3c73d7896746550")]
 [assembly: System.Reflection.AssemblyProductAttribute("Post.Query.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Post.Query.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
