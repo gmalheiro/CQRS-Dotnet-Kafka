6ac0453d5f58b88c3b8f4ea40c5547c8244cd191
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2023-07-14 16:12:37 -0300

    Added reference to EventStore in Program.cs

--- Code/SM-Post/Post.Cmd/Post.Cmd.Api/Program.cs
+++ Code/SM-Post/Post.Cmd/Post.Cmd.Api/Program.cs
@@ -1,9 +1,12 @@
+using CQRS.Core.Domain;
 using Post.Cmd.Infrastructure.Config;
+using Post.Cmd.Infrastructure.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.Configure<MongoDbConfig>(builder.Configuration.GetSection(nameof(MongoDbConfig)));
+builder.Services.AddScoped<IEventStoreRepository, EventStoreRepository>();
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
