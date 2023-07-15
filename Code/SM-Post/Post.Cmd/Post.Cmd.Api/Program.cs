bf706485534bae1569c811938cdee40431f776b7
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2023-07-15 15:04:39 -0300

    Event Producer class created

--- Code/SM-Post/Post.Cmd/Post.Cmd.Api/Program.cs
+++ Code/SM-Post/Post.Cmd/Post.Cmd.Api/Program.cs
@@ -1,3 +1,4 @@
+using Confluent.Kafka;
 using CQRS.Core.Domain;
 using CQRS.Core.Handlers;
 using CQRS.Core.Infrastructure;
@@ -13,6 +14,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.Configure<MongoDbConfig>(builder.Configuration.GetSection(nameof(MongoDbConfig)));
+builder.Services.Configure<ProducerConfig>(builder.Configuration.GetSection(nameof(ProducerConfig)));
 builder.Services.AddScoped<IEventStoreRepository, EventStoreRepository>();
 builder.Services.AddScoped<IEventStore, EventStore>();
 builder.Services.AddScoped<IEventSourcingHandler<PostAggregate>, EventSourcingHandler>();
